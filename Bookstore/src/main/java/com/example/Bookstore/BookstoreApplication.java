20fb3322f40d5ee846fa7fedc784551e3aaacd13
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2023-02-20 13:20:05 +0200

    Task 1- Bookstore: Add JPA and database
    a.) Add JPA support and H2 database to your bookstore project by modifying pom.xml file.
    b.) Create an entity from your Book class
    c.) Add command line runner to insert few example data to your database
    d.) Modify properties to show SQL statements in the console
    e.) Enable H2 console

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,7 +1,13 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -9,5 +15,16 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book book1 = new Book("Iron Widow","Xiran Jay Zhao",2021,"22259-89",20);
+			Book book2 = new Book("The Priory of the Orange Tree","Samantha Shannon",2020,"22251-88",18);
+			Book book3 = new Book("An Enchantment of Ravens","Margaret Rogerson",2017,"25559-77",12);		
+	repository.save(book1);
+	repository.save(book2);
+	repository.save(book3);
+	
+		};
+}
 }
--- Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
+++ Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,14 +1,65 @@
 package com.example.Bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
-	private String titl;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
 	private String author;
 	private Integer publicationYear;
 	private String isbn;
 	private Integer price;
+	
+	public Book() {
+		
+	}
+	
+	public Book(String title, String author, Integer publicationYear, String isbn, Integer price) {
+		super();
+		this.title=title;
+		this.author=author;
+		this.publicationYear=publicationYear;
+		this.price=price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
 
-	public void setTitl(String titl) {
-		this.titl = titl;
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public Integer getPublicationYear() {
+		return publicationYear;
+	}
+
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+
+	public Integer getPrice() {
+		return price;
+	}
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public void setAuthor(String author) {
@@ -29,7 +80,7 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Book [titl=" + titl + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
 				+ ", price=" + price + "]";
 	}
 
